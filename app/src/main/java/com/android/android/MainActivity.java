f8871dd4283d3d46c6989ef1d6518226cc837dcf
Denis <hahocok@gmail.com>
2020-11-09 06:29:48 +0300

    add basic lesson 3

--- app/src/main/java/com/android/android/MainActivity.java
+++ app/src/main/java/com/android/android/MainActivity.java
@@ -3,12 +3,91 @@ package com.android.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "My Log";
+
+    private MainPresenter presenter;
+    private TextView mainTemperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_city);
+        setContentView(R.layout.activity_main);
+
+        presenter = MainPresenter.getInstance();
+        mainTemperature = findViewById(R.id.main_temperature);
+
+        String instanceState;
+        if (savedInstanceState == null){
+            instanceState = "Первый запуск!";
+        }
+        else{
+            instanceState = "Повторный запуск!";
+        }
+        Toast.makeText(getApplicationContext(), instanceState + " - onCreate()", Toast.LENGTH_SHORT).show();
+        Log.d(LOG_TAG, instanceState + " - onCreate()");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+        Log.d(LOG_TAG, "onStart()");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState){
+        super.onRestoreInstanceState(saveInstanceState);
+        mainTemperature.setText(((Integer)presenter.getTemperature()).toString());
+        Toast.makeText(getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.d(LOG_TAG, "Повторный запуск!! - onRestoreInstanceState()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+        Log.d(LOG_TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+        Log.d(LOG_TAG, "onPause()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState){
+        super.onSaveInstanceState(saveInstanceState);
+        presenter.setTemperature(Integer.parseInt(mainTemperature.getText().toString()));
+        Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.d(LOG_TAG, "onSaveInstanceState()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+        Log.d(LOG_TAG, "onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+        Log.d(LOG_TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+        Log.d(LOG_TAG, "onStop()");
+    }
+
 }
---
+++ app/src/main/java/com/android/android/MainPresenter.java
@@ -0,0 +1,32 @@
+package com.android.android;
+
+public final class MainPresenter {
+    private static MainPresenter instance = null;
+    private static final Object syncObj = new Object();
+
+    // Это наш счетчик
+    private int temperature;
+
+    // Конструктор (вызывать извне его нельзя, поэтому он приватный)
+    private MainPresenter(){
+        temperature = 0;
+    }
+
+    // Увеличение счетчика
+    public void setTemperature(int value){
+        temperature = value;
+    }
+
+    public int getTemperature(){
+        return temperature;
+    }
+
+    public static MainPresenter getInstance(){
+        synchronized (syncObj) {
+            if (instance == null) {
+                instance = new MainPresenter();
+            }
+            return instance;
+        }
+    }
+}
